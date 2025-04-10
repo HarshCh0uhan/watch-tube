19380300bd1744ec1c7c49b89fb22c7607a1f23e
HarshCh0uhan <harshcoding2004@gmail.com>
2025-04-10 14:06:19 +0530

    Add Tailwind CSS and Remix Icon dependencies; restructure App component with new layout

---
+++ src/components/Head.js
@@ -0,0 +1,44 @@
+import { RiAccountCircleFill, RiMenu5Line, RiMicFill, RiMovieFill, RiNotification3Fill, RiSearchLine, RiVideoUploadFill } from "@remixicon/react";
+import React from "react";
+import App from "../App";
+
+const Head = () => {
+    return (
+        <div className="grid grid-flow-col justify-between h-[70px] pl-10 pr-10 m-auto shadow-lg items-center">
+            <div className="flex gap-5">
+                <button>
+                    <RiMenu5Line />
+                </button>
+                <button className="flex">
+                    <RiMovieFill size={30}/>
+                    <div className="text-xl">
+                        WatchTube
+                    </div>
+                </button>
+            </div>
+            <div className="flex gap-5">
+                <input type="text" className="w-72 h-9 rounded-3xl border-2"/>
+                <button>
+                    <RiSearchLine/>
+                </button>
+                <button>
+                    <RiMicFill/>
+                </button>
+            </div>
+            <div className="flex gap-5">
+                <button>
+                    <RiVideoUploadFill/>
+                </button>
+                <button>
+                    <RiNotification3Fill/>
+                </button>
+                <button>
+                    <RiAccountCircleFill size={30}/>
+                </button>
+
+            </div>
+        </div>
+    )
+}
+
+export default Head;
