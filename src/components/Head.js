05e304ed73c6a61fb07e153f62a63da6c40313bf
HarshCh0uhan <harshcoding2004@gmail.com>
2025-04-13 09:45:27 +0530

    optimizing search and its suggetions

--- src/components/Head.js
+++ src/components/Head.js
@@ -1,45 +1,51 @@
 import { RiAccountCircleFill, RiMenu5Line, RiMicFill, RiMovieFill, RiNotification3Fill, RiSearchLine, RiVideoUploadFill } from "@remixicon/react";
 import {toggleMenu} from "../utils/appSlice";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { YOUTUBE_SUGGETION_API } from "../utils/constants";
+import {cacheResults} from "../utils/searchSlice"
 
 const Head = () => {
 
     const [searchQuery, setSearchQuery] = useState("")
     const [suggetions, setSuggetions] = useState([])
     const [showSuggetions, setShowSuggetions] = useState(false)
+    const dispatch = useDispatch();
+    const searchCache = useSelector(store => store.search)
     
-    console.log(searchQuery);
+    // console.log(suggetions);
 
     const searchSuggetion = async () => {
-        console.log("API - " + searchQuery);
+        // console.log("API - " + searchQuery);
 
         const data = await fetch(YOUTUBE_SUGGETION_API + searchQuery);
 
         const json = await data.json();
 
-        console.log(json[1]);
+        // console.log(json[1]);
 
         setSuggetions(json[1])
+
+        dispatch(cacheResults({
+            [searchQuery]: json[1]
+        }))
     }
     
     useEffect(() => {
+        const timer = setTimeout(() => (searchCache[searchQuery]) ? setSuggetions(searchCache[searchQuery]) : searchSuggetion(), 200);
+        
         const handleScroll = () => setShowSuggetions(false);
         
         window.addEventListener('scroll', handleScroll);
-
-        const timer = setTimeout(() => searchSuggetion(), 200);
         
         return () => {
            window.removeEventListener('scroll', handleScroll);
            clearTimeout(timer)
-           console.log("cleared")
+        //    console.log("cleared")
        }
     }, [searchQuery])
     
-    const dispatch = useDispatch();
 
     const handleMenu = () => {
         dispatch(toggleMenu())
