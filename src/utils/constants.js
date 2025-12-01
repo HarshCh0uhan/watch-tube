8a558c957a0937b154bf12dfe3b0cf1f1872c9fa
Harsh_Chouhan <harshcoding2004@gmail.com>
2025-12-01 17:16:12 +0530

    Final Commit

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,4 @@
-const GOOGLE_API_KEY = "AIzaSyAiX94JM6Qisw-8WGwE_dypZF1pJj3-0ng"
-
-export const YOUTUBE_VIDEOS_API = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" + GOOGLE_API_KEY
+export const YOUTUBE_VIDEOS_API = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" + process.env.GOOGLE_API_KEY
 
 export const YOUTUBE_SUGGETION_API = "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q="
 
