471f2fa581a1fbbd5feda25808f52753ff8111be
Harsh_Chouhan <harshcoding2004@gmail.com>
2025-12-01 19:53:53 +0530

    Finally

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,6 +2,6 @@
 export const YOUTUBE_VIDEOS_API = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=${process.env.REACT_APP_GOOGLE_API_KEY}`;
 
 
-export const YOUTUBE_SUGGETION_API = "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q="
+export const YOUTUBE_SUGGETION_API = "https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q="
 
 export const OFFSET_LIVE_CHAT = 15;
